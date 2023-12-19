fa6b08351c795822cf57f4fe840584d76628542a
andrew9488 <pashkevichandrew@gmail.com>
2023-12-19 13:22:44 +0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-export const App = () => {
-  return <>"app"</>;
-};
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-export default App;
+import { Layout, AppRoutes } from "./shared";
+import { NotFound } from "./pages/not-found";
+import { Orders } from "./pages/orders";
+import { Order } from "./pages/order";
+
+export const App: React.FC = () => {
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path={AppRoutes.Orders} element={<Orders />} />
+                <Route path={AppRoutes.Order} element={<Order />} />
+
+                <Route path={AppRoutes.Companies} element={<div>{AppRoutes.Companies}</div>} />
+                <Route path={AppRoutes.Groups} element={<div>{AppRoutes.Groups}</div>} />
+                <Route path={AppRoutes.Users} element={<div>{AppRoutes.Users}</div>} />
+                <Route path={AppRoutes.Credits} element={<div>{AppRoutes.Credits}</div>} />
+                <Route path={AppRoutes.Documents} element={<div>{AppRoutes.Documents}</div>} />
+
+                <Route path={`${AppRoutes.Home}`} element={<Navigate to={AppRoutes.Orders} />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
+};
---
+++ src/shared/constants/app-routes.ts
@@ -0,0 +1,10 @@
+export enum AppRoutes {
+    Home = "/",
+    Orders = "/orders",
+    Companies = "/companies",
+    Groups = "/groups",
+    Users = "/users",
+    Credits = "/credits",
+    Documents = "/documents",
+    Order = "/orders/:id",
+}
